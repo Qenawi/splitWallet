08dc3beb02b49c6375cd3c9d80454c09ec64eab4
Qenawi <aldmanmero@yahoo.com>
2020-12-05 21:16:38 +0200

    init

---
+++ Tipsy/DetailsController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailsController.swift
+//  Tipsy
+//
+//  Created by Qenawi on 12/5/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+class DetailsControler: UIViewController
+{
+    @IBAction func reCalcUlatAction(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBOutlet var reCalcuate: [UIButton]!
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var amount: UILabel!
+    var amountVal = 0.0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.amount.text = String(amountVal)
+    }
+}
--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
+let nav = "mainToDetiails"
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valueText: UITextField!
+    @IBOutlet weak var twenty: UIButton!
+    @IBOutlet weak var tenBtn: UIButton!
+    @IBOutlet weak var zeroBtn: UIButton!
+    @IBOutlet weak var steeperLapel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    var amount:Double = 0.0
+    @IBAction func steeper(_ sender: UIStepper)
+    {
+        steeperLapel.text = String(sender.value)
+    }
+    @IBAction func navigate(_ sender: UIButton) {
+        self.performSegue(withIdentifier: nav, sender: self)
+    }
+    @IBAction func onAction(_ sender: UIButton)
+    {
+        if sender.tag == 0 {
+            zeroBtn.isSelected = true
+            tenBtn.isSelected = false
+            twenty.isSelected = false
+
+            
+        }
+        else if sender.tag == 10 {
+                       zeroBtn.isSelected = false
+                       tenBtn.isSelected = true
+                       twenty.isSelected = false
+        }
+       else  if sender.tag == 20 {
+                       zeroBtn.isSelected = false
+                       tenBtn.isSelected = false
+                       twenty.isSelected = true
+        }
+        self.caclulate()
+    }
+    @IBOutlet weak var textValue: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    func caclulate()
+    {
+        amount = Double(valueText.text ?? "0.0") ?? 0.0
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.identifier == nav {
+           let ee = (segue.destination as! DetailsControler)
+            ee.amountVal = amount
+        }
+    }
 
 }
 
